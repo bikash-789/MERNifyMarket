a7e725b1ae818492b9a4623428df38e7ebc5c095
Bikash Chauhan <chauhanbikash789@gmail.com>
2023-12-15 22:28:55 +0530

    Bug fix : user profile update method

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -11,24 +11,77 @@ exports.read = (req, res) => {
 };
 
 // UPDATE
+// exports.update = (req, res) => {
+//   // $set : will set the fields provided in req.body with new values
+//   // new: true; will provide the user with updated data
+//   User.findOneAndUpdate(
+//     { _id: req.profile._id },
+//     { $set: req.body },
+//     { new: true },
+//     (err, user) => {
+//       if (err) {
+//         res.status(400).json({
+//           error: "User not authorized",
+//         });
+//       }
+//       user.hashed_password = undefined;
+//       user.salt = undefined;
+//       res.status(200).json(user);
+//     }
+//   );
+// };
+
 exports.update = (req, res) => {
-  // $set : will set the fields provided in req.body with new values
-  // new: true; will provide the user with updated data
-  User.findOneAndUpdate(
-    { _id: req.profile._id },
-    { $set: req.body },
-    { new: true },
-    (err, user) => {
-      if (err) {
-        res.status(400).json({
-          error: "User not authorized",
+  const { newPassword } = req.body;
+
+  // If newPassword is provided, update the password
+  if (newPassword) {
+    // Step 1: Find the user based on the provided email
+    User.findOne({ _id: req.profile._id }, (err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          error: "User not found",
         });
       }
-      user.hashed_password = undefined;
-      user.salt = undefined;
-      res.status(200).json(user);
-    }
-  );
+
+      // Step 2: Set the new password using the virtual field
+      user.password = newPassword;
+
+      // Update the hashed_password field with the new hashed value
+      user.hashed_password = user.encryptPassword(newPassword);
+
+      // Save the user object to update the password
+      user.save((err, updatedUser) => {
+        if (err) {
+          return res.status(500).json({
+            error: "Error updating password",
+          });
+        }
+        // Clear sensitive information before sending the response
+        updatedUser.hashed_password = undefined;
+        updatedUser.salt = undefined;
+        res.status(200).json(updatedUser);
+      });
+    });
+  } else {
+    // If newPassword is not provided, update other details using findOneAndUpdate
+    User.findOneAndUpdate(
+      { _id: req.profile._id },
+      { $set: req.body },
+      { new: true },
+      (err, updatedUser) => {
+        if (err) {
+          return res.status(400).json({
+            error: "User not authorized",
+          });
+        }
+        // Clear sensitive information before sending the response
+        updatedUser.hashed_password = undefined;
+        updatedUser.salt = undefined;
+        res.status(200).json(updatedUser);
+      }
+    );
+  }
 };
 
 // READ by id
